a25c6060d693d66e9b095e22a848281ae40b2c1b
Amir M. M. Farid <amir.m.m.farid@gmail.com>
2026-07-21 10:01:09 -0400

    feat(v3-policy): add layered FEMU policy entry point

---
+++ FEMU-SxSSD/hw/femu/bbssd/policy/tee-ftl-v3-policy.c
@@ -0,0 +1,3 @@
+#define TEE_V2_POLICY 1
+#define TEE_V3_POLICY 1
+#include "tee-ftl-v1-policy.c"
---
+++ FEMU-SxSSD/hw/femu/bbssd/policy/tee-ftl-v3-policy.h
@@ -0,0 +1,5 @@
+#ifndef TEE_FTL_V3_POLICY_H
+#define TEE_FTL_V3_POLICY_H
+#include "tee-ftl-v2-policy.h"
+#include "tee/tee-v3-policy.h"
+#endif
